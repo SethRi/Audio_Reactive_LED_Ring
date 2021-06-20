987f4551bebcb745301ed8365262ab85e608bc8a
SethRi <48566116+SethRi@users.noreply.github.com>
2021-06-20 10:37:28 -0500

    Add files via upload

---
+++ python/install/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="Dancy-Pi",
+    version="1.0",
+    url="https://www.easyprogramming.net",
+    download_url="https://github.com/naztronaut/dancyPi-audio-reactive-led",
+    description="Audio Reactive Raspberry Pi with WS2812b LEDs.",
+    license="MIT",
+    install_requires=['numpy', 'pyaudio', 'pyqtgraph', 'scipy==1.4.1', 'rpi_ws281x']
+)
